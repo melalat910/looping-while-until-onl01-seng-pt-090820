894ee7cddf6c954700ed48872e100a4fe6e1af18
Tirzah P. Howard <tp.howard47@gmail.com>
2020-09-17 03:35:25 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,10 @@
 def using_while#your code here
   levitation_force = 6
+  # 1. Condition to be met
   while levitation_force == 10
-    puts "Wingardium Leviosa"
-    levitation_force +=1
-  end
+  # 2. Stuff to do while true
+  puts "Wingardium Leviosa"
+    # 3. How do we meet the condition
+  levitation_force +=1
+ end
 end

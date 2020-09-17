582c8559feef28fd79a5290cb458f12e8a72b551
Tirzah P. Howard <tp.howard47@gmail.com>
2020-09-17 03:26:08 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -4,7 +4,7 @@ def using_until#your code here
   until levitation_force == 10
   # 2. Stuff to do while true
   puts "Wingardium Leviosa"
-  # 3. How do we meet the condition 
+  # 3. How do we meet the condition
   levitation_force +=1
  end
 end
